982c86e354f66ec2680dafc0a69201446383c3b2
rahmanraja <rahman15692@gmail.com>
2018-02-15 13:32:20 +0530

    Verify Firstname and Lastname

--- src/main/java/org/cucumber/in/cucumber/DemoqaStep.java
+++ src/main/java/org/cucumber/in/cucumber/DemoqaStep.java
@@ -4,11 +4,13 @@ package org.cucumber.in.cucumber;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 
 public class DemoqaStep {
 	public WebDriver driver;
@@ -28,9 +30,14 @@ public class DemoqaStep {
 	}
 
 	@Then("^I verify entered firstName and lastname$")
-	public void i_verify_entered_firstName_and_lastname() throws Exception {
-		
-	   
+	public void i_verify_entered_firstName_and_lastname() throws Exception 
+	{
+		String fname = driver.findElement(By.id("name_3_firstname")).getAttribute("value");
+		Assert.assertEquals("Rajesh",fname );
+		System.out.println(fname);
+		String lname = driver.findElement(By.id("name_3_lastname")).getAttribute("value");
+		Assert.assertEquals("Kuppusamy",lname );
+		System.out.println(lname);
 	}
 
 
